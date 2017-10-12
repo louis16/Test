c4630245f2d13fa6aec3791e3ae8152b8783f605
liulin <louis16.liu@gmail.com>
2017-10-12 13:25:43 +0800

    修改第三次

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -9,7 +9,6 @@ public class HelloWorld {
   public String sayHelloWorldFrom(String from) {
     String result = "Hello, world, from " + from;
     System.out.println(result);
-    System.out.println("result");
     return result;
   }
   public static void main(String[] argv) {
